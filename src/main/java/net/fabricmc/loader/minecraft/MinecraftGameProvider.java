6fb0bda581391b08905f04cba07a4b82847695f9
i509VCB <git@i509.me>
2020-11-15 17:00:31 -0800

    Yeet Gson from loader (#306)
    * Move to JsonReader for parsing mod metadata
    
    Custom value error lol and deprcation
    
    Reading people in v0
    
    Read the links
    
    little things
    
    Handle dependency versions
    
    Hard yeet of gson, we just have gradle and cl exclusion
    
    Woops
    
    Throw exceptions when parsing version json
    
    Remove gson cl exclusion
    
    Obligatory renames
    
    Re add gson methods for now :(
    
    Optimize reading code
    
    Handle duplicate schemaVersion field if schemaVersion is first field
    
    Optimize parts of exhaustive schemaVersion search
    
    This fully delegates checks of duplicate mismatched `schemaVersion` values to the metadata parsers and inlines some logic to be more efficient.
    Actual catch new parsing errors
    
    spell correction
    
    And it's super-minimized gson now
    
    This commented aged like milk
    
    Use input stream reader instead
    
    Exclude gson from licenser
    
    Specify changes to Gson per apache 2.0
    
    More unit tests
    
    Adds testing code for custom values.
    
    Adds a typical mod test case.
    
    Fixed a bug where contact info would not read key of element before reading string value.
    Clean up testing folders and categorize tests
    
    Add long test file
    
    Fix some parsing errors found during testing
    
    Neaten up a bit of jar resolving
    
    Complain about schemaVersion position
    
    Move the headers up
    
    Fix parsing of entrypoints which makes unit tests work.
    
    Things should work, tested with a modpack
    
    * Readd gson CL exclusion for now
    
    * Cleanup two comments
    
    * Resolve review requests and add warning message system
    
    * Words
    
    * Add Fabric copyright to modified gson files
    
    * Apply suggestions from code review
    
    Co-authored-by: Pyrofab <redstoneinfire@gmail.com>
    
    * Drop added TODO markers from build.gradle
    
    Co-authored-by: Pyrofab <redstoneinfire@gmail.com>
    (cherry picked from commit 3b0e7cb906aa90fdad979d2f1c41e53f072faa3e)

--- src/main/java/net/fabricmc/loader/minecraft/MinecraftGameProvider.java
+++ src/main/java/net/fabricmc/loader/minecraft/MinecraftGameProvider.java
@@ -41,7 +41,6 @@ import java.util.List;
 import java.util.Optional;
 
 public class MinecraftGameProvider implements GameProvider {
-
 	private EnvType envType;
 	private String entrypoint;
 	private Arguments arguments;
